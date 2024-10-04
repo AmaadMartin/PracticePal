c65a219f91e42ddb7000aa7fa2fcfc30665af3d5
AmaadMartin <amaad0martin@gmail.com>
2024-10-04 12:45:11 -0400

    changed ip and added gitignore

--- api/api.py
+++ api/api.py
@@ -382,5 +382,5 @@ async def grade_quiz(payload: dict):
 
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0.", port=10000)
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0.", port=10000)
+    # uvicorn.run(app, host="localhost", port=8000)
